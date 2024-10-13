5bcf601c28913e6a97d2816d793e38434458fb41
arulabinsa <135586057+arulabinsa@users.noreply.github.com>
2024-10-13 18:51:32 +0530

    Add files via upload
    Day1 Assignment

---
+++ Chrome.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version = 91.0f;
+		System.out.println("Verion is "+version);
+		String developer = "Google";
+		System.out.println("Developer is"+developer);
+		boolean isBeta = false;
+		System.out.println("isBeta is" + isBeta);
+		int releaseYear = 2008;
+		System.out.println("realease year is"+releaseYear);
+		char shortcutkey ='C';
+		System.out.println("shortcutkey" + shortcutkey);
+
+	}
+
+}
